d6c288240801872d2c3e6830220f36cb7dfc4891
Wadd Anton <bondarev.1997@bk.ru>
2020-02-03 11:45:18 +0300

    fix echo

--- src/echo_script.c
+++ src/echo_script.c
@@ -6,7 +6,7 @@
 /*   By: wanton <wanton@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/31 10:18:40 by wanton            #+#    #+#             */
-/*   Updated: 2020/01/31 11:15:23 by wanton           ###   ########.fr       */
+/*   Updated: 2020/02/03 11:40:48 by wanton           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ static int	parse_flag(char **arg)
 	while (arg[i])
 	{
 		j = 1;
+		if (arg[i][0] != '-')
+			break ;
 		while (arg[i][j])
 		{
 			if (arg[i][j] != 'n')
@@ -37,12 +39,10 @@ static int	parse_flag(char **arg)
 int		echo_script(char **arg, char **env)
 {
 	int i;
-	int flag;
 
 	(void)env;
 	if ((i = parse_flag(arg)) == -1)
 		return (0);
-	flag = (i > 1 ? 1 : 0);
 	while (arg[i])
 	{
 		ft_putstr(arg[i++]);
